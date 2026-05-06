8b3712fb5800e9d4f269360e6c698fdd74191e3e
DANIELSUPREETH <DANIELSUPREETH>
2026-05-06 20:05:18 +0530

    fix route

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Quiz;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| These routes handle browser-based pages (UI).
-|
-*/
-
-// Home route (optional)
 Route::get('/', function () {
     return redirect('/quiz/1');
 });
 
-
-// Show quiz page
 Route::get('/quiz/{id}', function ($id) {
     $quiz = Quiz::with('questions.options')->findOrFail($id);
     return view('quiz', compact('quiz'));
